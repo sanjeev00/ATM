c113007b7cf5b5548ef5d2f410eee9f778f9c2bc
Sanjeev <sanjeevkrishnab@gmail.com>
2019-10-13 12:24:46 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -17,12 +17,18 @@ if($num>0)
 {
 	$row = mysqli_fetch_array($res);
 	echo mysqli_error($conn);
-	if($row['pin']!=$pin)
+	if($row['attempt']==0)
+	{
+		$flag = -1;
+	}
+	else if($row['pin']!=$pin)
 	{
 		$flag = 1;
 	}
 	else
 	{
+		$query = "UPDATE user set attempt=3 where cardno='$cardno'";
+		mysqli_query($conn,$query);
 		$_SESSION["auth"] = true;
 		$_SESSION["cardno"] = $cardno;
 		header('location: dash.php');
@@ -54,11 +60,23 @@ else
 		PIN <input type="password" name="pin" maxlength="4" minlength="4"><br>
 		<input type="submit" name="submit" value="Authenticate">
 		<br>
-		<?php if($flag==1)
+		<?php 
+		if($flag==-1)
+		echo "<p style='color:red'>Card blocked Contact bank</p>";
+		if($flag==1)
+		{
+		$att = $row['attempt']-1;
+		$query = "UPDATE user set attempt=$att where cardno='$cardno'";
+		mysqli_query($conn,$query);
 		echo "<p style='color:red'>invalid pin</p>";
+		if($att==0)
+			echo "<p style='color:red'>Card blocked Contact bank</p>";
+		else
+			echo "<p style='color:red'>".$att." attempts remmaining</p>";
+		}
 		if($flag==2)
 		echo "<p style='color:red'>No Card found</p>";
-?> 
+?> 	
 
 	</form>
 </fieldset>
